6664cceacd9819055fb1aa485954cded69e8660c
Sergio Spina <serspina@emeal.nttdata.com>
2025-06-26 11:40:08 +0200

    chore: hidden the page "Storico caricamenti"

--- src/pages/caricamenti/caricamenti.tsx
+++ src/pages/caricamenti/caricamenti.tsx
@@ -1,31 +1,27 @@
-import { useTranslation } from 'react-i18next';
 import { Box } from '@mui/material';
 import EmptyList from '../components/EmptyList';
 
-const Caricamenti = () => {
-  const { t } = useTranslation();
-  return (
+const Caricamenti = () => (
+  <Box
+    sx={{
+      display: 'grid',
+      gridTemplateColumns: 'repeat(12, 1fr)',
+      justifyContent: 'center',
+      width: '100%',
+      backgroundColor: 'white',
+      p: 2,
+    }}
+  >
     <Box
       sx={{
-        display: 'grid',
-        gridTemplateColumns: 'repeat(12, 1fr)',
+        display: 'inline',
+        gridColumn: 'span 12',
         justifyContent: 'center',
-        width: '100%',
-        backgroundColor: 'white',
-        p: 2,
+        textAlign: 'center',
       }}
     >
-      <Box
-        sx={{
-          display: 'inline',
-          gridColumn: 'span 12',
-          justifyContent: 'center',
-          textAlign: 'center',
-        }}
-      >
-        <EmptyList message={t('pages.initiativesList.emptyList')} />
-      </Box>
+      <EmptyList message={'Pagina non implementata'} />
     </Box>
-  );
-};
+  </Box>
+);
 export default Caricamenti;
