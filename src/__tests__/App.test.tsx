d7841023ccffb144bf209e5a9654341c4e3d0010
fragreco <fragreco@emeal.nttdata.com>
2025-07-15 11:48:22 +0200

    fix: tests

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,42 +1,115 @@
-/* istanbul ignore file */
-
 import App from '../App';
 import '../locale';
 import { renderWithContext } from '../utils/__tests__/test-utils';
+
+jest.mock('../utils/env', () => ({
+  URL_API: {
+    OPERATION: 'http://mock-api/register',
+  },
+  API_TIMEOUT_MS: 10000,
+}));
+
+jest.mock('../routes', () => ({
+  __esModule: true,
+  default: {
+    HOME: '/home'
+  },
+  BASE_ROUTE: '/base'
+}));
 
 jest.mock('@pagopa/mui-italia/dist/components/Footer/Footer', () => ({
   Footer: () => {},
 }));
 
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useLocation: () => ({
-    pathname: 'localhost:3000/registro-dei-beni',
-  }),
+jest.mock('../services/rolePermissionService', () => ({
+  getUserPermission: jest.fn(),
+  getPortalConsent: jest.fn(),
+  savePortalConsent: jest.fn(),
 }));
 
-const mockSignOutFn = jest.fn();
+jest.mock('../api/registerApiClient', () => ({
+  registerClient: {
+    get: jest.fn(),
+    post: jest.fn(),
+    put: jest.fn(),
+    delete: jest.fn(),
+  },
+}));
 
-jest.mock('../services/rolePermissionService');
-jest.mock('../decorators/withLogin');
-jest.mock('../decorators/withSelectedPartyProducts');
+jest.mock('../decorators/withLogin', () => (Component: any) => Component);
+jest.mock('../decorators/withSelectedPartyProducts', () => (Component: any) => Component);
+
+const mockAcceptTOS = jest.fn();
+
+jest.mock('../hooks/useTCAgreement', () => jest.fn());
+jest.mock('../helpers', () => ({
+  fetchUserFromLocalStorage: jest.fn(),
+}));
+
+import useTCAgreement from '../hooks/useTCAgreement';
+import { fetchUserFromLocalStorage } from '../helpers';
+import { INVITALIA } from '../utils/constants';
 
 beforeEach(() => {
   jest.spyOn(console, 'error').mockImplementation(() => {});
   jest.spyOn(console, 'warn').mockImplementation(() => {});
+  jest.clearAllMocks();
 });
 
 describe('Test suite for App component', () => {
-  test('Render App with TOS accepted', () => {
+  test('Render App with TOS accepted and standard user', () => {
+    (useTCAgreement as jest.Mock).mockReturnValue({
+      isTOSAccepted: true,
+      acceptTOS: mockAcceptTOS,
+      firstAcceptance: false,
+    });
+
+    (fetchUserFromLocalStorage as jest.Mock).mockReturnValue({
+      org_role: 'STANDARD',
+    });
+
+    renderWithContext(<App />);
+  });
+
+  test('Render App with TOS accepted and Invitalia user', () => {
+    (useTCAgreement as jest.Mock).mockReturnValue({
+      isTOSAccepted: true,
+      acceptTOS: mockAcceptTOS,
+      firstAcceptance: false,
+    });
+
+    (fetchUserFromLocalStorage as jest.Mock).mockReturnValue({
+      org_role: INVITALIA,
+    });
+
     renderWithContext(<App />);
   });
 
   test('Render App with TOS not accepted', () => {
-    jest.mock('../hooks/useTCAgreement', () => () => ({
-      isTOSAccepted: true,
-      acceptTOS: mockSignOutFn,
+    (useTCAgreement as jest.Mock).mockReturnValue({
+      isTOSAccepted: false,
+      acceptTOS: mockAcceptTOS,
+      firstAcceptance: true,
+    });
+
+    (fetchUserFromLocalStorage as jest.Mock).mockReturnValue({
+      org_role: 'STANDARD',
+    });
+
+    renderWithContext(<App />);
+  });
+
+  test('Render App with undefined TOS state', () => {
+    (useTCAgreement as jest.Mock).mockReturnValue({
+      isTOSAccepted: undefined,
+      acceptTOS: mockAcceptTOS,
       firstAcceptance: false,
-    }));
+    });
+
+    (fetchUserFromLocalStorage as jest.Mock).mockReturnValue({
+      org_role: 'STANDARD',
+    });
+
     renderWithContext(<App />);
   });
 });
--- src/utils/__tests__/test-utils.tsx
+++ src/utils/__tests__/test-utils.tsx
@@ -1,25 +1,26 @@
 import { ThemeProvider } from '@mui/material';
 import { theme } from '@pagopa/mui-italia';
 import { render } from '@testing-library/react';
-import { createMemoryHistory } from 'history';
 import React from 'react';
 import { Provider } from 'react-redux';
-import { Router } from 'react-router';
+import { MemoryRouter } from 'react-router-dom';
 import { createStore } from '../../redux/store';
 
 export const renderWithContext = (
-  element?: React.ReactNode,
-  injectedStore?: ReturnType<typeof createStore>,
-  injectedHistory?: ReturnType<typeof createMemoryHistory>
+    element?: React.ReactNode,
+    injectedStore?: ReturnType<typeof createStore>,
+    initialEntries: string[] = ['/']
 ) => {
-  const store = injectedStore ? injectedStore : createStore();
-  const history = injectedHistory ? injectedHistory : createMemoryHistory();
-  render(
-    <ThemeProvider theme={theme}>
-      <Router history={history}>
-        <Provider store={store}>{element}</Provider>
-      </Router>
-    </ThemeProvider>
-  );
-  return { store, history };
+    const store = injectedStore ?? createStore();
+
+    return {
+        ...render(
+            <ThemeProvider theme={theme}>
+                <MemoryRouter initialEntries={initialEntries}>
+                    <Provider store={store}>{element}</Provider>
+                </MemoryRouter>
+            </ThemeProvider>
+        ),
+        store,
+    };
 };
