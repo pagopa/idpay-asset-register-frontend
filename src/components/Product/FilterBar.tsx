2930fce43aed50b1ac5af618bbba21357b3f2bd2
Sergio Spina <serspina@emeal.nttdata.com>
2025-07-30 18:08:29 +0200

    RDB-129 feat: uses a spinner while loading data

--- src/components/Product/FilterBar.tsx
+++ src/components/Product/FilterBar.tsx
@@ -31,6 +31,7 @@ interface FilterProps {
   setGtinCodeFilter: Dispatch<SetStateAction<string>>;
   tableData: Array<ProductDTO>;
   handleDeleteFiltersButtonClick: () => void;
+  loading: boolean;
 }
 
 const FILTER_WIDTHS = {
@@ -78,119 +79,121 @@ export default function FilterBar({
   const handleDeleteFilters = () => handleDeleteFiltersButtonClick();
 
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'row',
-        gap: 1,
-        mb: 5,
-        width: '100%',
-        overflow: 'hidden',
-      }}
-    >
-      <FormControl size="small" sx={{ flexBasis: FILTER_WIDTHS.categoria }}>
-        <InputLabel id="category-filter-select-label">
-          {t('pages.products.filterLabels.category')}
-        </InputLabel>
-        <Select
-          labelId="category-filter-select-label"
-          id="category-filter-select"
-          value={categoryFilter}
-          label={t('pages.products.filterLabels.category')}
-          MenuProps={{ PaperProps: { style: { maxHeight: 350 } } }}
-          onChange={handleCategoryChange}
-        >
-          {Object.keys(PRODUCTS_CATEGORY).map((category) => (
-            <MenuItem key={category} value={t(`pages.products.categories.${category}`)}>
-              {t(`pages.products.categories.${category}`)}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-
-      <FormControl fullWidth size="small" sx={{ flexBasis: FILTER_WIDTHS.stato }}>
-        <InputLabel id="status-filter-select-label">
-          {t('pages.products.filterLabels.status')}
-        </InputLabel>
-        <Select
-          labelId="status-filter-select-label"
-          id="status-filter-select"
-          value={statusFilter}
-          label={t('pages.products.filterLabels.status')}
-          MenuProps={{ PaperProps: { style: { maxHeight: 350 } } }}
-          onChange={handleStatusChange}
-        >
-          {Object.keys(PRODUCTS_STATES).map((status) => (
-            <MenuItem key={status} value={t(`pages.products.categories.${status}`)}>
-              {t(`pages.products.categories.${status}`)}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-
-      <FormControl size="small" sx={{ flexBasis: FILTER_WIDTHS.lotto }}>
-        <InputLabel id="batch-filter-select-label">
-          {t('pages.products.filterLabels.batch')}
-        </InputLabel>
-        <Select
-          labelId="batch-filter-select-label"
-          id="batch-filter-select"
-          value={batchFilter}
-          label={t('pages.products.filterLabels.batch')}
-          MenuProps={{ PaperProps: { style: { maxHeight: 350 } } }}
-          onChange={handleBatchChange}
-          sx={{
-            paddingRight: '40px !important',
-          }}
-        >
-          {batchFilterItems?.map((batch) => (
-            <MenuItem key={batch?.productFileId} value={batch?.productFileId}>
-              {batch?.batchName}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-
-      <TextField
-        sx={{ flexBasis: FILTER_WIDTHS.eprel }}
-        size="small"
-        id="eprel-code-text"
-        label={t('pages.products.filterLabels.eprelCode')}
-        variant="outlined"
-        value={eprelCodeFilter}
-        onChange={handleEprelCodeChange}
-      />
-
-      <TextField
-        sx={{ flexBasis: FILTER_WIDTHS.gtin }}
-        size="small"
-        id="gtin-code-text"
-        label={t('pages.products.filterLabels.gtinCode')}
-        variant="outlined"
-        value={gtinCodeFilter}
-        onChange={handleGtinCodeChange}
-      />
-
-      <Button
-        disabled={noFilterSetted()}
-        variant="outlined"
-        sx={{ height: 44, minWidth: 100 }}
-        onClick={handleFilter}
-      >
-        {t('pages.products.filterLabels.filter')}
-      </Button>
-      <Button
-        disabled={noFilterSetted() && !errorStatus}
-        variant="text"
+    <>
+      <Box
         sx={{
-          height: 44,
-          flexBasis: FILTER_WIDTHS.rimuovi,
-          minWidth: 100,
+          display: 'flex',
+          flexDirection: 'row',
+          gap: 1,
+          mb: 5,
+          width: '100%',
+          overflow: 'hidden',
         }}
-        onClick={handleDeleteFilters}
       >
-        {t('pages.products.filterLabels.deleteFilters')}
-      </Button>
-    </Box>
+        <FormControl size="small" sx={{ flexBasis: FILTER_WIDTHS.categoria }}>
+          <InputLabel id="category-filter-select-label">
+            {t('pages.products.filterLabels.category')}
+          </InputLabel>
+          <Select
+            labelId="category-filter-select-label"
+            id="category-filter-select"
+            value={categoryFilter}
+            label={t('pages.products.filterLabels.category')}
+            MenuProps={{ PaperProps: { style: { maxHeight: 350 } } }}
+            onChange={handleCategoryChange}
+          >
+            {Object.keys(PRODUCTS_CATEGORY).map((category) => (
+              <MenuItem key={category} value={t(`pages.products.categories.${category}`)}>
+                {t(`pages.products.categories.${category}`)}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        <FormControl fullWidth size="small" sx={{ flexBasis: FILTER_WIDTHS.stato }}>
+          <InputLabel id="status-filter-select-label">
+            {t('pages.products.filterLabels.status')}
+          </InputLabel>
+          <Select
+            labelId="status-filter-select-label"
+            id="status-filter-select"
+            value={statusFilter}
+            label={t('pages.products.filterLabels.status')}
+            MenuProps={{ PaperProps: { style: { maxHeight: 350 } } }}
+            onChange={handleStatusChange}
+          >
+            {Object.keys(PRODUCTS_STATES).map((status) => (
+              <MenuItem key={status} value={t(`pages.products.categories.${status}`)}>
+                {t(`pages.products.categories.${status}`)}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        <FormControl size="small" sx={{ flexBasis: FILTER_WIDTHS.lotto }}>
+          <InputLabel id="batch-filter-select-label">
+            {t('pages.products.filterLabels.batch')}
+          </InputLabel>
+          <Select
+            labelId="batch-filter-select-label"
+            id="batch-filter-select"
+            value={batchFilter}
+            label={t('pages.products.filterLabels.batch')}
+            MenuProps={{ PaperProps: { style: { maxHeight: 350 } } }}
+            onChange={handleBatchChange}
+            sx={{
+              paddingRight: '40px !important',
+            }}
+          >
+            {batchFilterItems?.map((batch) => (
+              <MenuItem key={batch?.productFileId} value={batch?.productFileId}>
+                {batch?.batchName}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        <TextField
+          sx={{ flexBasis: FILTER_WIDTHS.eprel }}
+          size="small"
+          id="eprel-code-text"
+          label={t('pages.products.filterLabels.eprelCode')}
+          variant="outlined"
+          value={eprelCodeFilter}
+          onChange={handleEprelCodeChange}
+        />
+
+        <TextField
+          sx={{ flexBasis: FILTER_WIDTHS.gtin }}
+          size="small"
+          id="gtin-code-text"
+          label={t('pages.products.filterLabels.gtinCode')}
+          variant="outlined"
+          value={gtinCodeFilter}
+          onChange={handleGtinCodeChange}
+        />
+        <Button
+          disabled={noFilterSetted()}
+          variant="outlined"
+          sx={{ height: 44, minWidth: 100 }}
+          onClick={handleFilter}
+        >
+          {t('pages.products.filterLabels.filter')}
+        </Button>
+
+        <Button
+          disabled={noFilterSetted() && !errorStatus}
+          variant="text"
+          sx={{
+            height: 44,
+            flexBasis: FILTER_WIDTHS.rimuovi,
+            minWidth: 100,
+          }}
+          onClick={handleDeleteFilters}
+        >
+          {t('pages.products.filterLabels.deleteFilters')}
+        </Button>
+      </Box>
+    </>
   );
 }
