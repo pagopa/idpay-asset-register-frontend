05ea50af6d8853e13019eeba7c6b15bd84be99b9
Mario De Fazio <mario11.defazio@emeal.nttdata.com>
2025-09-04 10:45:39 +0200

    tooltip su products table

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -49,3 +49,5 @@ export const maxLengthEmail: number = 40;
 export const maxLengthOverviewProd: number = 75;
 export const maxLengthOverviewInvit: number = 140;
 export const MAX_LENGTH_DETAILL_PR: number = 40;
+export const MAX_LENGTH_TABLE_PR: number = 15;
+
