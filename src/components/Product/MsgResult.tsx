6084953c9abc5bfdca9486d669fef74ff038e32c
Mario De Fazio <mario11.defazio@emeal.nttdata.com>
2025-09-23 11:36:53 +0200

    shadows msgResult

--- src/components/Product/MsgResult.tsx
+++ src/components/Product/MsgResult.tsx
@@ -41,7 +41,14 @@ const MsgResult: React.FC<MsgResultProps> = ({
         zIndex: 9999,
       }}
     >
-      <Box sx={{ width: '395px', overflowX: 'auto' }}>
+      <Box
+        sx={{
+          width: '395px',
+          overflowX: 'auto',
+          boxShadow:
+            '0px 1px 10px 0px #002B551A, 0px 4px 5px 0px #002B550D, 0px 2px 4px -1px #002B551A',
+        }}
+      >
         <Alert severity={severity} variant={variant}>
           {message}
           {children}
