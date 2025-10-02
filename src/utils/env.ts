0bf683cebbfd0ea2b79be01c1710210eeb0d3c27
fragreco <fragreco@emeal.nttdata.com>
2025-10-02 14:43:15 +0200

    fix: env.ts

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -11,7 +11,7 @@ export const ENV = {
   },
 
   URL_FE: {
-    PRE_LOGIN: env.get('REACT_APP_URL_FE_PRE_LOGIN').asString(),
+    PRE_LOGIN: env.get('REACT_APP_URL_FE_PRE_LOGIN').required().asString(),
     LOGIN: env.get('REACT_APP_URL_FE_LOGIN').required().asString(),
     LOGOUT: env.get('REACT_APP_URL_FE_LOGOUT').required().asString(),
     LANDING: env.get('REACT_APP_URL_FE_LANDING').asString(),
