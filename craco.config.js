5ec574430f39a153156693bd08fc4fd174027055
fragreco01 <fragreco@emeal.nttdata.com>
2026-03-20 12:00:44 +0100

    fix: tests

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,33 @@
 module.exports = {
+  jest: {
+    configure: (jestConfig) => {
+      jestConfig.transformIgnorePatterns = [
+        '/node_modules/(?!(@pagopa/selfcare-common-frontend|@pagopa/ts-commons|@pagopa/mui-italia|@mui|@fontsource|node-fetch|mixpanel-browser)/)',
+        '/node_modules/@pagopa/selfcare-common-frontend/lib/config/env\\.js$',
+      ];
+
+      jestConfig.moduleNameMapper = {
+        ...(jestConfig.moduleNameMapper || {}),
+        '^.+\\.(css|sass|scss)$': 'identity-obj-proxy',
+        '^@pagopa/selfcare-common-frontend/lib/config/env$': '<rootDir>/src/__mocks__/selfcare-env.js',
+        '^@pagopa/selfcare-common-frontend/lib/config/env.js$': '<rootDir>/src/__mocks__/selfcare-env.js',
+        '^@pagopa/selfcare-common-frontend/lib/config/env\\.js$': '<rootDir>/src/__mocks__/selfcare-env.js',
+        '^@pagopa/selfcare-common-frontend/lib/config/env\\.mjs$': '<rootDir>/src/__mocks__/selfcare-env.js',
+        '^@pagopa/selfcare-common-frontend/lib/config/env\\.cjs$': '<rootDir>/src/__mocks__/selfcare-env.js',
+        '^@pagopa/selfcare-common-frontend/lib/services/analyticsService$':
+          '<rootDir>/src/__mocks__/selfcare-analyticsService.ts',
+        '^@pagopa/selfcare-common-frontend/lib/services/analyticsService\\.js$':
+          '<rootDir>/src/__mocks__/selfcare-analyticsService.ts',
+      };
+
+      jestConfig.transform = {
+        ...(jestConfig.transform || {}),
+        '^.+\\.[jt]sx?$': 'babel-jest',
+      };
+
+      return jestConfig;
+    },
+  },
   webpack: {
     configure: {
       module: {
---
+++ src/__mocks__/selfcare-env.js
@@ -0,0 +1,34 @@
+module.exports = {
+  CONFIG: {
+    LOGIN_ADMIN_GOOGLE: '',
+    LOGIN_ADMIN: '',
+    LOGIN_ADMIN_URL: '',
+    LOGIN_PAGOPA_URL: '',
+    LOGOUT_URL: '',
+    ASSISTANCE_EMAIL: '',
+    ENABLE_LOGIN: false,
+    MIXPANEL_TOKEN: '',
+    MIXPANEL_API_HOST: '',
+    HEADER: {
+      LINK: {
+        PRODUCTURL: '',
+      },
+    },
+  },
+  default: {
+    LOGIN_ADMIN_GOOGLE: '',
+    LOGIN_ADMIN: '',
+    LOGIN_ADMIN_URL: '',
+    LOGIN_PAGOPA_URL: '',
+    LOGOUT_URL: '',
+    ASSISTANCE_EMAIL: '',
+    ENABLE_LOGIN: false,
+    MIXPANEL_TOKEN: '',
+    MIXPANEL_API_HOST: '',
+    HEADER: {
+      LINK: {
+        PRODUCTURL: '',
+      },
+    },
+  },
+};
