42867f0387a217cb84ad7734fe53c4099db214d9
fragreco01 <fragreco@emeal.nttdata.com>
2026-03-23 17:10:35 +0100

    fix: openapi.register.yml update (#160)

--- openApi/scripts/api-register_fixPreGen.js
+++ openApi/scripts/api-register_fixPreGen.js
@@ -2,56 +2,28 @@
 const regexReplace = require('regex-replace');
 
 regexReplace(
-  '"items": \\{\\},',
-  '"items":{"type": "object"},',
-  'openApi/generated/register-swagger20.json',
-  {
-    fileContentsOnly: true,
-  }
+    '"items": \\{\\},',
+    '"items":{"type": "object"},',
+    'openApi/generated/register-swagger20.json',
+    {
+        fileContentsOnly: true,
+    }
 );
 
 regexReplace(
-  '"type": "array"',
-  '"type": "array","items":{"type":"object"}',
-  'openApi/generated/register-swagger20.json',
-  { fileContentsOnly: true }
+    '"rewardRule": \\{\\},',
+    '"rewardRule":{"type": "object"},',
+    'openApi/generated/register-swagger20.json',
+    {
+        fileContentsOnly: true,
+    }
 );
 
 regexReplace(
-  '"type":"array"',
-  '"type":"array","items":{"type":"object"}',
-  'openApi/generated/register-swagger20.json',
-  { fileContentsOnly: true }
-);
-
-regexReplace(
-  '"items":{"properties":',
-  '"items":{"type":"object","properties":',
-  'openApi/generated/register-swagger20.json',
-  { fileContentsOnly: true }
-);
-
-regexReplace(
-  '"items": {"properties":',
-  '"items": {"type":"object","properties":',
-  'openApi/generated/register-swagger20.json',
-  { fileContentsOnly: true }
-);
-
-regexReplace(
-  '"rewardRule": \\{\\},',
-  '"rewardRule":{"type": "object"},',
-  'openApi/generated/register-swagger20.json',
-  {
-    fileContentsOnly: true,
-  }
-);
-
-regexReplace(
-  '"operationId": "returns-fixed-automated-criteria",',
-  '"operationId":"returnsFixedAutomatedCriteria",',
-  'openApi/generated/register-swagger20.json',
-  {
-    fileContentsOnly: true,
-  }
+    '"operationId": "returns-fixed-automated-criteria",',
+    '"operationId":"returnsFixedAutomatedCriteria",',
+    'openApi/generated/register-swagger20.json',
+    {
+        fileContentsOnly: true,
+    }
 );
