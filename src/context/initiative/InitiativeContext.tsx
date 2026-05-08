3131fc2a6419c0d0a0123da8a19e8feb662e112c
fragreco01 <fragreco@emeal.nttdata.com>
2026-05-08 12:01:05 +0200

    fix: Bnd 86 fix (#177)

--- src/context/initiative/InitiativeContext.tsx
+++ src/context/initiative/InitiativeContext.tsx
@@ -33,31 +33,35 @@ export const InitiativeProvider = ({ children }: InitiativeProviderProps) => {
   const [initiatives, setInitiatives] = useState<Array<InitiativeDTO>>([]);
   const [isLoadingInitiatives, setIsLoadingInitiatives] = useState(false);
 
+  // eslint-disable-next-line sonarjs/cognitive-complexity
   useEffect(() => {
-    const controller = new AbortController();
+      const controller = new AbortController();
 
-    void (async () => {
-      try {
-        setIsLoadingInitiatives(true);
-        const response = await getMerchantInitiativeList();
-        if (!controller.signal.aborted) {
-          setInitiatives(response?.data ?? []);
+      void (async () => {
+        if (initiativeId !== "000") {
+          try {
+            setIsLoadingInitiatives(true);
+            const response = await getMerchantInitiativeList();
+            if (!controller.signal.aborted) {
+              setInitiatives(response?.data ?? []);
+            }
+          } catch {
+            if (!controller.signal.aborted) {
+              setInitiatives([]);
+            }
+          } finally {
+            if (!controller.signal.aborted) {
+              setIsLoadingInitiatives(false);
+            }
+          }
         }
-      } catch {
-        if (!controller.signal.aborted) {
-          setInitiatives([]);
-        }
-      } finally {
-        if (!controller.signal.aborted) {
-          setIsLoadingInitiatives(false);
-        }
-      }
-    })();
+      })();
 
-    return () => {
-      controller.abort();
-    };
-  }, []);
+      return () => {
+        controller.abort();
+      };
+    },
+    []);
 
   const value = useMemo<InitiativeContextValue>(
     () => ({ initiativeId, initiatives, isLoadingInitiatives }),
