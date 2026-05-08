312313fbb92a946da5e933812043bef0f91b4668
EleonoraC96 <cossu.eleonora1996@gmail.com>
2026-05-08 15:43:18 +0200

    fix: fixed categories keys

--- src/hooks/useCategories.tsx
+++ src/hooks/useCategories.tsx
@@ -11,10 +11,12 @@ export const useCategories = () => {
     const { t } = useScopedTranslation();
     const namespace = t("categories", { returnObjects: true}) as Record<string, string>;
     const categories: Record<string, CategoryType> = isObject(namespace) ? Object?.entries(namespace).reduce((acc, [key, value]) => {
-        const isNotCookinghobs = namespace?.eprel && key !== "cookinghobs";
-        const csvNamespace = t(`${isNotCookinghobs ? "eprel" : "csv"}`, { returnObjects: true, category: value }) as { headers: Array<string>; fields: Array<string> };
+        const formattedValue = value.toLowerCase().replace(/[ ,]+/g, '-');
+        const isEprel = !!namespace?.eprel;
+        const isNotCookinghobs = key !== "cookinghobs";
+        const csvNamespace = t(`${isNotCookinghobs && isEprel ? "eprel" :  "csv"}`, { returnObjects: true, category: value }) as { headers: Array<string>; fields: Array<string> };
         const csvFile = createCsv(csvNamespace);
-        return { ...acc, [key]: { label: value, csv: { name: `${isNotCookinghobs ? "eprel" : key}_template.csv`, file: csvFile } } };
+        return { ...acc, [key]: { label: value, csv: { name: `${isNotCookinghobs && isEprel ? "eprel" : isNotCookinghobs ? formattedValue : key}_template.csv`, file: csvFile } } };
     }, {}) : [];
     return { categories };
 };
