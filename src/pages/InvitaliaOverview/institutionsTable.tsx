61442c25956742de869708a57b3c2dd3dd3da2ce
Francesco Fico <Francesco.Fico@nttdata.com>
2026-06-05 17:49:56 +0200

    fix overflow and fix chip in product table

--- src/pages/InvitaliaOverview/institutionsTable.tsx
+++ src/pages/InvitaliaOverview/institutionsTable.tsx
@@ -136,7 +136,11 @@ const InstitutionsTable: React.FC<InstitutionsTableProps> = ({
   const goToInstitutionPage = (institution: Institution) => {
     dispatch(setInstitution(institution));
     navigate(buildRoute(ROUTES.INVITALIA_PRODUCTS_LIST, initiativeId ?? ''), {
-      state: { organizationId: institution.institutionId },
+      state: {
+        organizationId: institution.institutionId,
+        organizationLabel: institution.description,
+        sourceInitiativeId: initiativeId,
+      },
     });
   };
 
